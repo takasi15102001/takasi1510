36ad907530a79256a2fe94b1ca6705180f875a44
takasi <takasi@gmail.com>
2022-11-29 21:40:26 +0700

    takasi

--- web.js
+++ web.js
@@ -14,7 +14,7 @@ $(document).ready( function () {
             Q0_3 = result["q0_3"];
 
             if(Q0_0 == 1 ){
-                document.getElementById("state1").src = 'https://th.bing.com/th?q=AC+Motor+Icons&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-WW&cc=VN&setlang=vi&adlt=moderate&t=1&mw=247';
+                document.getElementById("state1").src = './img/motor.jpg';
             }
             else {
                 document.getElementById("state1"). src = 'https://thumbs.dreamstime.com/z/motor-pump-irrigation-icon-outline-style-motor-pump-irrigation-icon-outline-motor-pump-irrigation-vector-icon-web-design-135519421.jpg';
